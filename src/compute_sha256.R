87e3419755d1f4e4ff2623db2f8c2a73ffce5193
florisvdh <floris.vanderhaeghe@inbo.be>
2019-05-10 16:07:58 +0200

    Improve script that is basis for a filehash function

--- src/compute_sha256.R
+++ src/compute_sha256.R
@@ -4,16 +4,19 @@ library(tidyverse)
 mypath <- "data"
 
 hashes <-
-    tibble(File = str_c(mypath, "/",
+    tibble(filepath = str_c(mypath, "/",
         list.files(path = mypath,
             recursive = TRUE)
       )) %>%
-    mutate(Connection = map(File, file),
+    mutate(filename = str_match(filepath, "(.+\\/)*(.+)")[,3],
+           Connection = map(filepath, file),
            SHA256 = map(Connection, function(x) {
                                         sha256(x) %>% str_c(collapse = '')
                                         }) %>% as.character
            ) %>%
-    select(File, SHA256)
+    select(filepath,
+           filename,
+           SHA256)
 
 hashes %>%
     write_csv("hashes.csv")
