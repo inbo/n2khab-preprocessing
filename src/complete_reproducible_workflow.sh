31278c3e5fc9c880373269fe1f0247b02d2efe76
florisvdh <floris.vanderhaeghe@inbo.be>
2020-04-21 18:14:24 +0200

    Repro workflow: wrap R functions in shell commands *
    This makes it renv-proof: R sessions are launched from the respective
    subdirectories, resulting in loading the renv-created .Rprofile,
    if present.

--- src/complete_reproducible_workflow.sh
+++ src/complete_reproducible_workflow.sh
@@ -1,12 +1,11 @@
-## This R script lists the steps in order, required to RE-generate processed
+## This shell script lists the steps in order, required to RE-generate processed
 ## (large or binary) data in the 'n2khab_data/20_processed'
 ## subfolder. It requires the RAW binary data to be present in the
 ## 'n2khab_data/10_raw' subfolder in the specified way (for further information,
 ## run: vignette("v020_datastorage", package = "n2khab")).
 
 ## Note on the relative paths: this script is supposed to be run from
-## the root of the n2khab-preprocessing repository. Advice: simply run it from the
-## n2khab-preprocessing.Rproj RStudio project.
+## the root of the n2khab-preprocessing repository.
 
 ## !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! #
 ## WARNING ! This script may take a very LONG time to run. You are advised
@@ -21,7 +20,7 @@
 # Generating the data folder inside this repository
 # -------------------------------------------------
 
-n2khab::filemanag_folders(root = "git")
+Rscript -e 'n2khab::filemanag_folders(root = "git")'
 
 
 # Generating GeoTIFF and GPKG files, derived from the GRTSmaster_habitats data source
@@ -29,40 +28,46 @@ n2khab::filemanag_folders(root = "git")
 
 # Be warned: this can take > 10 hours.
 
-setwd("src/generate_GRTS_10_GRTSmh_base4frac")
-rmarkdown::render_site(output_format = 'bookdown::html_document2',
-                       encoding = 'UTF-8')
+(cd src/generate_GRTS_10_GRTSmh_base4frac;\
+Rscript -e "rmarkdown::render_site(output_format = 'bookdown::html_document2',\
+                       encoding = 'UTF-8')"\
+)
 
-setwd("src/generate_GRTS_20_GRTSmh_brick")
-rmarkdown::render_site(output_format = 'bookdown::html_document2',
-                       encoding = 'UTF-8')
+(cd src/generate_GRTS_20_GRTSmh_brick;\
+Rscript -e "rmarkdown::render_site(output_format = 'bookdown::html_document2',\
+                       encoding = 'UTF-8')"\
+)
 
-setwd("src/generate_GRTS_30_GRTSmh_diffres")
-rmarkdown::render_site(output_format = 'bookdown::html_document2',
-                       encoding = 'UTF-8')
+(cd src/generate_GRTS_30_GRTSmh_diffres;\
+Rscript -e "rmarkdown::render_site(output_format = 'bookdown::html_document2',\
+                       encoding = 'UTF-8')"\
+)
 
 
 # Generating habitatmap_stdized
 # ------------------------------
 
-setwd("src/generate_habitatmap_stdized")
-rmarkdown::render_site(output_format = 'bookdown::html_document2',
-                       encoding = 'UTF-8')
+(cd src/generate_habitatmap_stdized;\
+Rscript -e "rmarkdown::render_site(output_format = 'bookdown::html_document2',\
+                       encoding = 'UTF-8')"\
+)
 
 # Generating watersurfaces_hab
 # ------------------------------
 
-setwd("src/generate_watersurfaces_hab")
-rmarkdown::render_site(output_format = 'bookdown::html_document2',
-                       encoding = 'UTF-8')
+(cd src/generate_watersurfaces_hab;\
+Rscript -e "rmarkdown::render_site(output_format = 'bookdown::html_document2',\
+                       encoding = 'UTF-8')"\
+)
 
 
 # Generating habitatmap_terr
 # ------------------------------
 
-setwd("src/generate_habitatmap_terr")
-rmarkdown::render_site(output_format = 'bookdown::html_document2',
-                       encoding = 'UTF-8')
+(cd src/generate_habitatmap_terr;\
+Rscript -e "rmarkdown::render_site(output_format = 'bookdown::html_document2',\
+                       encoding = 'UTF-8')"\
+)
 
 
 
